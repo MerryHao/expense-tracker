0196567ec428daf680ca77189d1d30cfe8d5bacc
MerryHao <mq6721@gmail.com>
2023-02-17 19:24:08 +0800

    feat: add totalAmount

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,7 +5,13 @@ router.get('/', (req, res) => {
   const userId = req.user._id
   Record.find({ userId })
     .lean()
-    .then(records => res.render('index', { records }))
+    .then(records => {
+      let totalAmount = 0
+      for(let i = 0; i < records.length; i++) {
+        totalAmount += records[i].amount
+      }
+      return res.render('index', { records, totalAmount })
+    })
     .catch(error => console.log(error))
 })
 module.exports = router
